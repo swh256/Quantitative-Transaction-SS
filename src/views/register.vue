768fbd9890e4a2061c882370d29b6326f76e6052
Shiri <1050721142@qq.com>
2021-07-15 23:28:46 +0800

    css添加

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,6 +1,60 @@
 <template>
 <div class="register">
-  <h1>REGISTER</h1>
+  <!-- form section start -->
+  <section class="w3l-workinghny-form">
+    <!-- /form -->
+    <div class="workinghny-form-grid">
+      <div class="wrapper">
+        <div class="logo">
+          <h1><a class="brand-logo" href="index.html"><span>Working</span> Sign In</a></h1>
+          <!-- if logo is image enable this
+              <a class="brand-logo" href="#index.html">
+                  <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+              </a> -->
+        </div>
+        <div class="workinghny-block-grid">
+          <div class="workinghny-left-img align-end">
+            <img src="../assets/images/2.png" class="img-responsive" alt="img" />
+          </div>
+          <div class="form-right-inf">
+
+            <div class="login-form-content">
+              <h2>Where to?</h2>
+              <form action="#" class="signin-form" method="post">
+                <div class="one-frm">
+
+                  <label>E-mail</label>
+                  <input type="text" name="E-mail"  placeholder="" required="">
+                </div>
+                <div class="one-frm">
+                  <label>Password</label>
+                  <input type="password" name="Password"  placeholder="" required="">
+                </div>
+                <label class="check-remaind">
+                  <input type="checkbox">
+                  <span class="checkmark"></span>
+                  <p class="remember">Remember Me</p>
+
+                </label>
+                <button class="btn btn-style mt-3">Sign In </button>
+                <p class="already">Don't have an account? <a href="#signin">Sign Up</a></p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- //form -->
+    <!-- copyright-->
+    <div class="copyright text-center">
+      <div class="wrapper">
+        <p class="copy-footer-29">© 2020 Working Sign In. All rights reserved | Design by <a
+            href="https://w3layouts.com">W3layouts</a></p>
+      </div>
+    </div>
+    <!-- //copyright-->
+  </section>
+  <!-- //form section start -->
 </div>
 </template>
 
@@ -10,6 +64,4 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped src="../css/login.css"></style>
