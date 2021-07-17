6da183288bb62976049b51018eeb2ac4c57d9451
CQU-code-lover <1209895097@qq.com>
2021-07-17 18:38:24 +0800

    add support : user_api , Authenticator (used to check permission and maintain login session)

--- backend/api/user_api.py
+++ backend/api/user_api.py
@@ -2,27 +2,65 @@
 # 一系列业务功能方法
 
 # 导入用户操作类
-from operation.user_operation  import User_Operation
+from backend.operation.user_operation import *
 
-from utils.data_proccess import *
 
 
 def User_list():
-    # 数据验证
-    # 功能具体操作
-    # 返回数据
-    # 包装数据
-    # return 数据
-    user_p = User_Operation()
-    result_data = user_p._all()
-    # result_data 复杂对象
-    # 转化
-    result =  Class_To_Data(result_data,user_p.__fields__)
-    return result 
+    return UserOperation().getUserList()
 
 
-def User_login(username,pwd):
-    return re_data
+# status code : 0 login fail     1 login success     2 have been login before
+def User_login(email,pwd):
+    # check this user is login?
+    authenticator = Authenticator()
+    if(authenticator.checkLogin()):
+        return 2
+    user = UserOperation()._queryByEmailPasswd(email,pwd)
+    if(user!=None):
+        # login OK
+        # set login info to session
+        authenticator._setUser(user)
+        return 1
+    else:
+        return 0
 
-def User_reg():
-    return re_data
+
+# return    0  fail      1 success
+def User_reg(email, pwd):
+    userOperator = UserOperation()
+    if(userOperator._queryByEmail(email)!=None):
+        # this email have been register
+        return 0
+    else:
+        userOperator.addUser(email,pwd)
+        return 1
+
+# return 1  (always invoked successful.)
+def User_logout():
+    authenticator = Authenticator()
+    if(authenticator.checkLogin()):
+        authenticator._clearUser()
+    return 1
+
+
+# return     0   fail         1  success
+def User_unreg(email,pwd):
+    userOperator = UserOperation()
+    user:User = userOperator._queryByEmailPasswd(email,pwd)
+    if(user==None):
+        # error email or pwd
+        return 0
+    userOperator.removeUser(user.userId)
+    return 1
+
+def User_change(dict:dict):
+    userID = dict.get("userID")
+    if(userID==None):
+        return 0
+    userOperation=UserOperation()
+    user = userOperation._queryByUserID(userID)
+    if(user==None):
+        return 0
+    userOperation._undumpTo(dict,user)
+    userOperation._commit()
--- backend/operation/user_operation.py
+++ backend/operation/user_operation.py
@@ -1,10 +1,11 @@
 # 导入对应数据模型类
 from backend.models.user import User
 from backend.db_config import db_init as db
+from flask import session
 
 
 # 用户模块操作类
-class User_Operation():
+class UserOperation():
     # 应该映射到user表的字段
     def __init__(self):
         pass
@@ -12,22 +13,56 @@ class User_Operation():
     # 操作1 获取所有用户
     def _getAll(self):
         return User.query.all()
+    def _commit(self):
+        db.session.commit()
+    def _queryByUserID(self, userID: int):
+        users = User.query.filterBy(User.userId == userID)
+        if (users != []):
+            return users[0]
+        else:
+            return None
 
-    def _queryByUserID(self, userID):
-        return User.query.filterBy(User.userId == userID)
+    def _queryByEmail(self, email: str):
+        users = User.query.filterBy(User.email == email)
+        if (users != []):
+            return users[0]
+        else:
+            return None
 
-    def _queryByEmail(self, email):
-        return User.query.filterBy(User.email == email)
+    def _queryByEmailPasswd(self, email: str, pwd: str):
+        users = User.query.filterBy(User.email == email, User.userPassword == pwd)
+        if (users != []):
+            return users[0]
+        else:
+            return None
 
-    def _queryByEmailPasswd(self, email, pwd):
-        return User.query.filterBy(User.email == email, User.userPassword == pwd)
-
-    def _insert(self, user):
+    def _insert(self, user: User):
         db.session.add(user)
         db.session.commit()
 
-    def addUser(self, email, password):
-        if (len(self._queryByEmail(email)) == 0):
+    def _dump(self, user: User):
+        dict = {}
+        dict["userId"] = user.userId
+        dict["userAuthority"] = user.userAuthority
+        dict["userPassword"] = user.userPassword
+        dict["email"] = user.email
+        dict["capitalReady"] = user.capitalReady
+        return dict
+
+
+    def _undumpTo(self, d:dict , user:User):
+        user.userId = d.get("userId")
+        user.userAuthority = d.get("userAuthority")
+        user.userPassword = d.get("userPassword")
+        user.email = d.get("email")
+        user.capitalReady = d.get("capitalReady")
+        return user
+
+    def _undump(self, d:dict):
+        return  self._undumpTo(d,User())
+
+    def addUser(self, email: str, password: str):
+        if (not self._queryByEmail(email)):
             # OK
             user = User()
             user.userPassword = password
@@ -39,22 +74,92 @@ class User_Operation():
         else:
             return False
 
-    def removeUser(self, userID):
-        u_list = self._queryByUserID(userID)
-        if (len(u_list) == 0):
+    def removeUser(self, userID: int):
+        user = self._queryByUserID(userID)
+        if (user):
             # the user is not exist
-            u_list[0].delete
+            user.delete()
             return True
         else:
             return False
 
-    def getCapital(self, userID):
-        u_list = self._queryByUserID(userID)
-        if (len(u_list) == 0):
+    def getCapital(self, userID: int):
+        user = self._queryByUserID(userID)
+        if (user):
             # the user is not exist
-            return u_list[0].capitalReady
+            return user.capitalReady
         else:
             return None
 
     def getUserList(self):
-        return self._getAll()
+        l = []
+        users = self._getAll()
+        for user in users:
+            l.append(self._dump(user))
+        return l
+
+    def getUserInfoByUserID(self, userID: str):
+        user = self._queryByUserID(userID)
+        if (user):
+            return self._dump(user)
+        else:
+            return None
+
+    def getUserInfoByEmail(self, email: str):
+        user = self._queryByEmail(email)
+        if (user):
+            return self._dump(user)
+        else:
+            return None
+
+    def getUserInfoByEmailPasswd(self, email: str, password: str):
+        user = self._queryByEmailPasswd(email, password)
+        if (user):
+            return self._dump(user)
+        else:
+            return None
+
+
+class SessionHandler():
+    def __init__(self, session):
+        self.session = session
+
+    def _set(self, key, val):
+        self.session[key] = val
+
+    def _get(self, key):
+        return self.session.get(key)
+
+    def _delete(self, key):
+        return self.session.pop(key, None)
+
+
+class Authenticator:
+    def __init__(self):
+        self.sessionHandler = SessionHandler(session)
+
+    def _setUser(self, user: User):
+        self.sessionHandler._set("user", user)
+
+    def _clearUser(self, user: User):
+        self.sessionHandler._delete("user")
+
+    def _getUser(self):
+        self.sessionHandler._get("user")
+
+    def getPermissionLevel(self):
+        user: User = self._getUser()
+        if (user == None):
+            # not login
+            return -1
+        else:
+            return user.userAuthority
+
+    def checkUser(self):
+        return self.getPermissionLevel() >= 0
+
+    def checkAdmin(self):
+        return self.getPermissionLevel() >= 1
+
+    def checkLogin(self):
+        return self.checkUser()
