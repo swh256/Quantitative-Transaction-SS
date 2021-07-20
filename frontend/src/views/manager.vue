6da17ceaf643824f9cc09e5be240c15da472db88
SireZBY <1050721142@qq.com>
2021-07-20 17:31:40 +0800

    管理界面完成、表格完善

--- frontend/src/views/manager.vue
+++ frontend/src/views/manager.vue
@@ -2,21 +2,21 @@
 <div class="manager">
   <HeadNav></HeadNav>
 <!--  <h1><el-button @click="Push_User_Info">Manager</el-button></h1><hr>-->
-  <el-container style="height: 500px; border: 1px solid #eee">
+  <el-container style="height: 750px; border: 1px solid #eee">
     <el-aside width="200px" style="background-color: #ffffff">
-      <el-menu :default-openeds="['1', '3']" :default-active="actIndex" @select="Menu_Select">
+      <el-menu :default-openeds="subOpen">
         <el-submenu index="1">
           <template slot="title"><i class="el-icon-user"></i>用户管理</template>
           <el-menu-item-group style="background-color: #ffffff">
-            <el-menu-item index="1-1">基本信息表</el-menu-item>
-            <el-menu-item index="1-2">持仓信息表</el-menu-item>
+            <el-menu-item index="1-1"  @click="Change_Show">基本信息表</el-menu-item>
+            <el-menu-item index="1-2" @click="Change_Show">持仓信息表</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
         <el-submenu index="2">
-          <template slot="title"><i class="el-icon-menu"></i>策略管理/上传</template>
+          <template slot="title"><i class="el-icon-menu"></i>策略管理</template>
           <el-menu-item-group style="background-color: #ffffff">
-            <el-menu-item index="2-1">策略列表管理</el-menu-item>
-            <el-menu-item index="2-2">上传策略模型</el-menu-item>
+            <el-menu-item index="2-1" @click="Change_Show">策略列表管理</el-menu-item>
+            <el-menu-item index="2-2" @click="Change_Show">已售策略</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
       </el-menu>
@@ -25,7 +25,7 @@
     <el-container>
       <el-header style="text-align: right; font-size: 12px">
         <el-dropdown>
-          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <i class="el-icon-setting" style="margin-right: 15px;"></i>
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item>查看</el-dropdown-item>
             <el-dropdown-item>新增</el-dropdown-item>
@@ -36,7 +36,7 @@
       </el-header>
 
       <el-main>
-        <el-table :data="tableData" v-if="true">
+        <el-table :data="tableData" v-if="actIndex=='1-1'">
           <el-table-column prop="id" label="用户id" width="140">
           </el-table-column>
           <el-table-column prop="email" label="邮箱" width="140">
@@ -49,39 +49,37 @@
           </el-table-column>
         </el-table>
         <el-table :data="tableData" v-else-if="actIndex=='1-2'">
+          <el-table-column prop="id" label="持仓id" width="140">
+          </el-table-column>
           <el-table-column prop="id" label="用户id" width="140">
           </el-table-column>
-          <el-table-column prop="email" label="邮箱" width="140">
+          <el-table-column prop="email" label="股票代码" width="140">
           </el-table-column>
-          <el-table-column prop="userPassword" label="密码" width="120">
+          <el-table-column prop="userPassword" label="股票数量" width="120">
           </el-table-column>
-          <el-table-column prop="userAuthority" label="用户权限">
+          <el-table-column prop="userAuthority" label="买入价">
           </el-table-column>
-          <el-table-column prop="capitalReady" label="预备投资金额" width="120">
+          <el-table-column prop="capitalReady" label="买入总价" width="120">
           </el-table-column>
         </el-table>
         <el-table :data="tableData" v-else-if="actIndex=='2-1'">
-          <el-table-column prop="id" label="用户id" width="140">
+          <el-table-column prop="id" label="策略id" width="140">
           </el-table-column>
-          <el-table-column prop="email" label="邮箱" width="140">
+          <el-table-column prop="id" label="策略名称" width="140">
           </el-table-column>
-          <el-table-column prop="userPassword" label="密码" width="120">
+          <el-table-column prop="email" label="策略价格" width="140">
           </el-table-column>
-          <el-table-column prop="userAuthority" label="用户权限">
-          </el-table-column>
-          <el-table-column prop="capitalReady" label="预备投资金额" width="120">
+          <el-table-column prop="userPassword" label="策略简介" width="140">
           </el-table-column>
         </el-table>
         <el-table :data="tableData" v-else-if="actIndex=='2-2'">
-          <el-table-column prop="id" label="用户id" width="140">
+          <el-table-column prop="id" label="已售id" width="140">
           </el-table-column>
-          <el-table-column prop="email" label="邮箱" width="140">
+          <el-table-column prop="email" label="策略id" width="140">
           </el-table-column>
-          <el-table-column prop="userPassword" label="密码" width="120">
+          <el-table-column prop="userPassword" label="用户id" width="120">
           </el-table-column>
-          <el-table-column prop="userAuthority" label="用户权限">
-          </el-table-column>
-          <el-table-column prop="capitalReady" label="预备投资金额" width="120">
+          <el-table-column prop="capitalReady" label="有效期" width="120">
           </el-table-column>
         </el-table>
       </el-main>
@@ -105,13 +103,14 @@ export default {
     return{
       actIndex:'',
       tableData: [],
+      subOpen:['1','2'],
     }
   },
   methods:{
-    Menu_Select(){
-      
+    Change_Show(event){
+      this.actIndex = event.index
     },
-    Push_User_Info(){
+    Get_User_Info(){
       axios.get('http://192.168.43.95:5000/manager/getUserInfoList')
           .then(data => {
             if (data.status == 200) {
